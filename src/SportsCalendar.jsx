ab686c1a8edf2b047097e9c261ba86a23ad84384
andrewfx2 <109618359+andrewfx2@users.noreply.github.com>
2025-08-25 19:05:48 -0400

    Update SportsCalendar.jsx

--- src/SportsCalendar.jsx
+++ src/SportsCalendar.jsx
@@ -237,7 +237,7 @@ const HockeyCardCalendar = () => {
               marginLeft: '12px',
               whiteSpace: 'nowrap'
             }
-          }, "Hockey Card Release Calendar")
+          }, "Release Calendar")
         ),
         
         // Right side: Action buttons
